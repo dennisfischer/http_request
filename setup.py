4a99d1078cab89e9fdfa42cb8d1bc48689cc1373
Dennis Fischer <chaosking0912@gmail.com>
2016-09-04 12:47:15 +0200

    Changed email

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     url='https://github.com/dennisfischer/http_request',
     license='MIT',
     author='Dennis Fischer',
-    author_email='chaosking0912@gmail.com',
+    author_email='dennis.fischer@live.com',
     description='A small python library to parse and build HTTP requests'
 )
