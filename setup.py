70207506a3f0b6e9e044bd45ab34c43a6cbfba7d
Dennis Fischer <chaosking0912@gmail.com>
2016-09-04 12:04:44 +0200

    Added HTTP Request parser code

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='http_request',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/dennisfischer/http_request',
+    license='MIT License',
+    author='Dennis Fischer',
+    author_email='dennis.fischer@live.com',
+    description='A small python library to parse and build HTTP requests'
+)
