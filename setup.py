9126c381ef64ee47d4a3a5a5bbd6bdee81dfe77f
Dennis Fischer <chaosking0912@gmail.com>
2016-09-28 15:31:50 +0200

    Updated setup py

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
-from distutils.core import setup
+import os
+from setuptools import setup
 
 setup(
     name='http_request',
-    version='1.1',
+    version='1.2',
     packages=['http_request'],
     url='https://github.com/dennisfischer/http_request',
     license='MIT',
     author='Dennis Fischer',
     author_email='dennis.fischer@live.com',
-    description='A small python library to parse and build HTTP requests'
+    description='A small python library to parse and build HTTP requests',
+	install_requires=[
+		"future"
+	],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
 )
